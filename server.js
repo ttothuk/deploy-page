0a0ea2a8aaaca4f45e280dbe4738c18fdaece1d5
Tibor Toth <40400971@live.napier.ac.uk>
2020-08-02 22:27:34 +0100

    trying to get heroku to work wih my stupid server

--- server.js
+++ server.js
@@ -3,32 +3,24 @@ if (process.env.NODE_ENV !== 'production'){
 }
 
 const express = require('express');
-const articleRouter = require('./routes/articles');
 const app = express();
-const mongoose = require('mongoose');
-const Article = require('./models/article');
-const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = (require('mongoose'))
 mongoose.connect((process.env.DB_URL), {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', error => console.log('connected to mongoose'))
 
-app.set('view engine', 'ejs');
-
-
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-
-
-app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({
-        createdAt: 'desc'
-    });
-    res.render('articles/index', { articles: articles }).catch((e) => { console.error(e); process.exit(1) });
-});
-
-
-app.use('/articles', articleRouter);
+app.use('/', indexRouter)
 
 
 app.listen(process.env.PORT || 3000)
